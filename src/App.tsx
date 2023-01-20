6347baba4b6e816aedff55acfcdc298f6f4c928a
Hagar Fisher <hagarfisher@gmail.com>
2023-01-20 22:20:41 +0200

    Add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,96 @@
 import "./App.css";
 import Canvas from "./components/Canvas";
 import { imgSources } from "./assets/imgSorces";
-function App() {
-  function pixelateImage(
-    canvas: HTMLCanvasElement,
-    context: CanvasRenderingContext2D | null,
-    originalImage: HTMLImageElement | null,
-    pixelationFactor: number
-  ) {
-    if (context === null || originalImage === null) {
-      return;
-    }
-    const originalWidth = originalImage.width;
-    const originalHeight = originalImage.height;
-    canvas.width = originalWidth;
-    canvas.height = originalHeight;
-    context.drawImage(originalImage, 0, 0, originalWidth, originalHeight);
-    const originalImageData = context.getImageData(
-      0,
-      0,
-      originalWidth,
-      originalHeight
-    ).data;
-    if (pixelationFactor !== 0) {
-      for (let y = 0; y < originalHeight; y += pixelationFactor) {
-        for (let x = 0; x < originalWidth; x += pixelationFactor) {
-          // extracting the position of the sample pixel
-          const pixelIndexPosition = (x + y * originalWidth) * 4;
-          // drawing a square replacing the current pixels
-          context.fillStyle = `rgba(
-              ${originalImageData[pixelIndexPosition]},
-              ${originalImageData[pixelIndexPosition + 1]},
-              ${originalImageData[pixelIndexPosition + 2]},
-              ${originalImageData[pixelIndexPosition + 3]}
-            )`;
-          const rectangleWidth = pixelationFactor;
-          const rectangleHeight = pixelationFactor;
-          context.fillRect(x, y, rectangleWidth, rectangleHeight);
-        }
+import { useEffect, useState } from "react";
+import EndRoundMessage from "./components/EndRoundMessage";
+
+const App = () => {
+  const initialGuessCount = 3;
+  const [score, setScore] = useState(0);
+  const pixelationFactors = [5, 7, 9];
+  const [guessCount, setGuessCount] = useState(initialGuessCount);
+  const [guess, setGuess] = useState("");
+  const [currentImg, setCurrentImg] = useState(
+    imgSources[Math.floor(Math.random() * imgSources.length)]
+  );
+  const [newRound, setNewRound] = useState(false);
+  const [message, setMessage] = useState("");
+  const [pixelationFactor, setPixelationFactor] = useState(
+    pixelationFactors[pixelationFactors.length - 1]
+  );
+  const validateGuess = () => {
+    if (currentImg.name.toLowerCase() === guess.toLowerCase()) {
+      setMessage("you guessed it!");
+      setScore((score) => score + 1);
+      setNewRound(true);
+      setCurrentImg(imgSources[Math.floor(Math.random() * imgSources.length)]);
+      setGuessCount(initialGuessCount);
+    } else {
+      if (guessCount === 1) {
+        setMessage("maybe next time...");
+        setNewRound(true);
+        setCurrentImg(
+          imgSources[Math.floor(Math.random() * imgSources.length)]
+        );
+        setGuessCount(initialGuessCount);
+      } else {
+        setGuessCount((count) => count - 1);
       }
     }
-  }
+    setGuess("");
+  };
+
+  useEffect(() => {
+    setPixelationFactor(pixelationFactors[guessCount - 1]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [guessCount]);
 
   return (
-    <div>
-      <h1>Guess The Movie!</h1>
-      <Canvas pixelateImage={pixelateImage} imgSrc={imgSources[0].src} />
-      <input type="text" placeholder="Enter a guess" />
-    </div>
+    <>
+      <div className="title-container">
+        <h1 className="title">Guess The Movie!</h1>
+      </div>
+      {newRound ? (
+        <EndRoundMessage
+          message={message}
+          delay={1000}
+          setNewRound={setNewRound}
+        />
+      ) : (
+        <div className="container">
+          <div className="game-info">
+            <h4>{guessCount} guesses left</h4>
+            <span>score: {score}</span>
+          </div>
+          <div className="canvas-container">
+            <Canvas
+              pixelationFactor={pixelationFactor}
+              imgSrc={currentImg.src}
+            />
+          </div>
+          <div className="input-container">
+            <input
+              onChange={(e) => setGuess(e.target.value)}
+              type="text"
+              placeholder="Enter a guess"
+              value={guess}
+            />
+            <button
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  validateGuess();
+                }
+              }}
+              onClick={validateGuess}
+              className="button"
+            >
+              Go
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
-}
+};
 
 export default App;
