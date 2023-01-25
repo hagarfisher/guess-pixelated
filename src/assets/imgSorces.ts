0d56108aad277c24e967bfcf0bb99713badeb813
Hagar Fisher <hagarfisher@gmail.com>
2023-01-25 16:29:59 +0200

    removing images from assets

--- src/assets/imgSorces.ts
+++ src/assets/imgSorces.ts
@@ -12,19 +12,19 @@ export const imgSources = [
     src: "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
   },
   {
-    name: "12 Angry Men",
+    name: "12 angry men",
     src: "https://m.media-amazon.com/images/M/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg",
   },
   {
-    name: "Pulp Fiction",
+    name: "pulp fiction",
     src: "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
   },
   {
-    name: "The Good the Bad and the Ugly",
+    name: "the good the bad and the ugly",
     src: "https://m.media-amazon.com/images/M/MV5BNjJlYmNkZGItM2NhYy00MjlmLTk5NmQtNjg1NmM2ODU4OTMwXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg",
   },
   {
-    name: "Forrest Gump",
+    name: "forrest gump",
     src: "https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
   },
 ];
